25716060a3afa9223344a5449467b5df6a59e39b
sarithasuk <sarithasuk83@gmail.com>
2016-01-20 21:18:14 -0800

    Initial Commit

---
+++ TestFile/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestFile
+//
+//  Created by Saritha Parsoya on 1/20/16.
+//  Copyright © 2016 org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestFile/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestFile
+//
+//  Created by Saritha Parsoya on 1/20/16.
+//  Copyright © 2016 org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
